562edfda58d3c4b10ef80fd9d567fe4ff0e214a8
MaggieZ111119 <maggiey.zhang@mail.utoronto.ca>
2024-12-01 03:03:00 -0500

    Data cleaning such as remove NAs according to context, select columns to study, change date time etc.

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,17 +1,21 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw plane data by Open Data Toronto
+# Author: Maggie Zhang
+# Date: 30 November 2024
+# Contact: maggiey.zhang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Have the data downloaded
+# Any other information needed? No
 
 #### Workspace setup ####
 library(tidyverse)
+library(dplyr)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("data/01-raw_data/raw_fire_data.csv")
+
+str(raw_data)
+
 
 cleaned_data <-
   raw_data |>
